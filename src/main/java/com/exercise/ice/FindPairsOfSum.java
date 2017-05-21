d993a6f47b91c84a11096d0897f8a57c981231db
williamdengnewyork <williamdeng@yahoo.com>
2017-05-21 16:34:38 -0400

    for sum value of 7

--- src/main/java/com/exercise/ice/FindPairsOfSum.java
+++ src/main/java/com/exercise/ice/FindPairsOfSum.java
@@ -1,6 +1,4 @@
 package com.exercise.ice;
-
-
 /*
 Problem: 
 Write me a program that that accepts an array of integers, and prints out the pairs of these integers that sum to 7.  
@@ -8,36 +6,76 @@ For example, If the input is [1, 2, 3, 4, 5, 6], the output could be (1,6), (2, 
 Different combinations of the same pair of numbers e.g, (1, 6) and (6, 1) are not distinct.
 */
 
+import java.util.Arrays;
+
 public class FindPairsOfSum {
-	static int sorttime=0;
-	static int time=0;
+	private static int time=0;
+    private static final int MAX = 100000;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Assumption: Input array of unsorted integers
-		//int[] a = {6}; 		
-		int[] a = {6,1,5,3,4,2,9,11,13,8,7}; 		
-		int sumValue = 13;
-
-		searchPairsBySum(a, sumValue);
-		System.out.println("Time Complexity - sorting: " + sorttime );
-		System.out.println("Time Complexity - search : " + time );
+		int sumValue = 7;
+		int[] a = {6,1,5,-3,4,2,9,11,13,8,0,7,16,10,3};  // unsorted array 		
 		
+		System.out.println("Array Size     : " + a.length );
+		System.out.println("Sum Value = " + sumValue);
+
+		System.out.println("Solution 1 - work for unsorted array"); 
+		time=0;
+		searchPairsBySum(a, sumValue); 
+		System.out.println("Time Complexity: " + time );
+				
+		
+		System.out.println("Solution 2 - pre-sorted array beforehand"); 
+		time=0;
+		Arrays.sort(a);
+		searchPairsBySumInSortedArray(a, sumValue);  
+		System.out.println("Time Complexity: " + time );
 	}
 
-	public static void searchPairsBySum(int[] a, int target){
+	// O(n)
+    static void searchPairsBySum(int[] a, int sum){
 		if (a == null || a.length == 0){
 			System.out.println("Warning: Array is null or has no element!");
 			return;
 		}
-		if (a.length == 1){
+		if (a.length < 2){
 			System.out.println("Warning: Array has less than 2 elements!");
 			return;
+		}		
+        boolean[] binmap = new boolean[MAX]; 
+		boolean[] binmapNegative=new boolean[MAX];
+		for (int i = 0; i < a.length; i++) {
+			int temp = sum - a[i];
+			if (temp < 0) {
+				if (binmapNegative[Math.abs(temp)]) {
+					System.out.println("(" + temp + "," + a[i] + ")");
+				}
+			} else {
+				if (binmap[temp] ) {
+					System.out.println("(" + temp + "," + a[i] + ")");
+				}
+			}
+
+			if (a[i] < 0) {
+				binmapNegative[Math.abs(a[i])] = true;
+			} else {
+				binmap[a[i]] = true;
+			}
+			time += 1;
 		}
-		
-		System.out.println("Array Size: " + a.length );		
-		quickSort(a, 0, a.length-1);
-		
+    }
+    
+    //If a[] is already sorted, this implementation is faster than O(n) on average
+	public static void searchPairsBySumInSortedArray(int[] a, int target){
+		if (a == null || a.length == 0){
+			System.out.println("Warning: Array is null or has no element!");
+			return;
+		}
+		if (a.length < 2){
+			System.out.println("Warning: Array has less than 2 elements!");
+			return;
+		}		
 		int i=0;
 		int l=a.length-1;		
 		while (i<l){
@@ -54,45 +92,5 @@ public class FindPairsOfSum {
 		}
 	}
 	
-	//quick sort
-	public static void quickSort(int[] arr, int low, int high) { //O(NlogN)
-		if (arr == null || arr.length == 0)
-			return;
- 		
-		if (low >= high)
-			return;
- 
-		// pick the pivot
-		int middle = low + (high - low) / 2;
-		int pivot = arr[middle];
- 
-		// make left < pivot and right > pivot
-		int i = low, j = high;
-		while (i <= j) {
-			sorttime +=1;
-			while (arr[i] < pivot) {
-				i++;
-			}
- 
-			while (arr[j] > pivot) {
-				j--;
-			}
- 
-			if (i <= j) {
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-				i++;
-				j--;
-			}
-		}
- 
-		// recursively sort two sub parts
-		if (low < j)
-			quickSort(arr, low, j);
- 
-		if (high > i)
-			quickSort(arr, i, high);
-	}
 
 }
---
+++ src/main/java/com/exercise/ice/SortingUtil.java
@@ -0,0 +1,93 @@
+package com.exercise.ice;
+
+public class SortingUtil {
+
+	// mergeSort
+	public static int[] mergeSort(int[] list) {
+		if (list.length <= 1) {
+			return list;
+		}
+		// 1. Split the array in two sub-arrays
+		int[] first = new int[list.length / 2];
+		int[] second = new int[list.length - first.length];
+		System.arraycopy(list, 0, first, 0, first.length);
+		System.arraycopy(list, first.length, second, 0, second.length);
+
+		// 2. Sort each half sub-array -- !!!! Recursive function call
+		mergeSort(first);
+		mergeSort(second);
+
+		// 3. Merge the halves together, overwriting the original array "list"
+		merge(first, second, list);
+		return list;
+	}
+
+	// static proc method
+	private static void merge(int[] first, int[] second, int[] result) {
+		// Merge both halves into the result array
+		// Next element to consider in the first array
+		int iFirst = 0;
+		// Next element to consider in the second array
+		int iSecond = 0;
+
+		// Next open position in the result
+		int j = 0;
+		// As long as neither iFirst nor iSecond is past the end, move the
+		// smaller element into the result.
+		while (iFirst < first.length && iSecond < second.length) {
+			if (first[iFirst] < second[iSecond]) {
+				result[j] = first[iFirst];
+				iFirst++;
+			} else {
+				result[j] = second[iSecond];
+				iSecond++;
+			}
+			j++;
+		}
+		// copy what's left
+		System.arraycopy(first, iFirst, result, j, first.length - iFirst);
+		System.arraycopy(second, iSecond, result, j, second.length - iSecond);
+	}
+
+	// quick sort
+	public static void quickSort(int[] arr, int low, int high) { // O(NlogN)
+		if (arr == null || arr.length == 0)
+			return;
+
+		if (low >= high)
+			return;
+
+		// pick the pivot
+		int middle = low + (high - low) / 2;
+		int pivot = arr[middle];
+
+		// make left < pivot and right > pivot
+		int i = low, j = high;
+		while (i <= j) {
+			while (arr[i] < pivot) {
+				i++;
+			}
+
+			while (arr[j] > pivot) {
+				j--;
+			}
+
+			if (i <= j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+		}
+
+		// recursively sort two sub parts
+		if (low < j)
+			quickSort(arr, low, j);
+
+		if (high > i)
+			quickSort(arr, i, high);
+	}
+
+	
+}
