a9ae345f5ccbab1ccffe8697257fe6966aada588
williamdengnewyork <williamdeng@yahoo.com>
2017-05-21 12:46:20 -0400

    check in

---
+++ src/main/java/com/exercise/ice/Anagrams.java
@@ -0,0 +1,83 @@
+package com.exercise.ice;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Anagrams {
+
+	// convert a given word into a string of its sorted characters
+	// e.g. "cat", "act", "tac" => "act"
+	public static String sortCharacter(String word) {
+		word.trim();
+		// select anyone of the following three sorting implementations
+		return sortCharacter1(word);
+	}
+
+	// Java 8 stream and sorted
+	public static String sortCharacter1(String word) {
+		return Stream.of(word.split("")).sorted().collect(Collectors.joining());
+	}
+
+	// Java collection sort() function
+	public static String sortCharacter2(String word) {
+		char[] cword = word.toCharArray();
+		java.util.Arrays.sort(cword);
+		return String.valueOf(cword);
+	}
+
+	// insertion sorting
+	public static String sortCharacter3(String word) {
+		char[] cword = word.toCharArray();
+		for (int i = 0; i < (cword.length - 1); i++) {
+			for (int j = i + 1; j > 0; j--) {
+				if (cword[j] < cword[j - 1]) {
+					char temp = cword[j - 1];
+					cword[j - 1] = cword[j];
+					cword[j] = temp;
+				}
+			}
+		}
+		return String.valueOf(cword);
+	}
+
+
+	// Add word to HashMap, using its sorted characters string as key,
+	// so all Anagrams of the sorted characters string are in Value by the same key 
+	public static void addToMap2(Map<String, StringBuilder> anagramMap, String sortedChars, String word) {
+		if (!anagramMap.containsKey(sortedChars)) { // new sorted characters
+			anagramMap.put(sortedChars, (new StringBuilder(word)));
+		} else { // sorted characters string already in map
+			anagramMap.put(sortedChars, anagramMap.get(sortedChars).append(", " + word));
+			// its a anagram group when there's more than word
+			anagramGroupSet.add(sortedChars);
+		}
+	}
+
+	final static String WordFileName = "src/main/resources/EnglishWords.txt";
+	final static Map<String, StringBuilder> anagramMap = new HashMap<String, StringBuilder>();
+	final static Set<String> anagramGroupSet = new TreeSet<String>(); 	// unique set of anagram group index
+
+	
+	public static void main(String[] args) throws IOException {
+		int wordcnt = 0;
+		for (String word : java.nio.file.Files.readAllLines(Paths.get(WordFileName), StandardCharsets.UTF_8)) {
+			addToMap2(anagramMap, sortCharacter(word), word);
+			wordcnt +=1;
+		}
+
+		for (String anagramIndex : anagramGroupSet) {
+			System.out.println(anagramIndex + " - " + anagramMap.get(anagramIndex));
+		}
+
+		System.out.println("Total number of words         : " + wordcnt);
+		System.out.println("Total number of anagram groups: " + anagramGroupSet.size());
+	}
+
+}
---
+++ src/main/java/com/exercise/ice/FindPairsOfSum.java
@@ -0,0 +1,85 @@
+package com.exercise.ice;
+
+
+public class FindPairsOfSum {
+	static int sorttime=0;
+	static int time=0;
+
+	public static void main(String[] args) {
+
+		// Assumption: Input array of unsorted integers
+		int[] a = {6,1,5,3,4,2,9,11,13,8,7}; 		
+		int sumValue = 13;
+
+		searchPairsBySum(a, sumValue);
+		System.out.println("Time Complexity - sorting: " + sorttime );
+		System.out.println("Time Complexity - search : " + time );
+		
+	}
+
+	public static void searchPairsBySum(int[] a, int target){
+		if (a == null || a.length == 0){
+			System.out.println("Warning: Array is null or has no element!");
+			return;
+		}
+		
+		System.out.println("Array Size: " + a.length );		
+		quickSort(a, 0, a.length-1);
+		
+		int i=0;
+		int l=a.length-1;		
+		while (i<l){
+			time +=1;
+			if (a[i]+a[l]==target){
+				System.out.println("(" + a[i] + "," + a[l] + ")");
+				i += 1;
+				l -= 1;
+			}else if(a[i]+a[l] > target){
+				l -= 1;
+			}else{ // (a[i]+a[l] < target)
+				i +=1;
+			}			
+		}
+	}
+
+	public static void quickSort(int[] arr, int low, int high) { //O(NlogN)
+		if (arr == null || arr.length == 0)
+			return;
+ 		
+		if (low >= high)
+			return;
+ 
+		// pick the pivot
+		int middle = low + (high - low) / 2;
+		int pivot = arr[middle];
+ 
+		// make left < pivot and right > pivot
+		int i = low, j = high;
+		while (i <= j) {
+			sorttime +=1;
+			while (arr[i] < pivot) {
+				i++;
+			}
+ 
+			while (arr[j] > pivot) {
+				j--;
+			}
+ 
+			if (i <= j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+		}
+ 
+		// recursively sort two sub parts
+		if (low < j)
+			quickSort(arr, low, j);
+ 
+		if (high > i)
+			quickSort(arr, i, high);
+	}
+
+}
